a1ba508e71fb0bb83aaa84fe9334d5d285f2af3f
Guillaume Paris <guillaume.paris@filigran.io>
2025-09-22 21:04:57 +0200

    [all] Rename pyobas to pyoaev

--- nuclei/test/nuclei_contracts/test_external_contracts.py
+++ nuclei/test/nuclei_contracts/test_external_contracts.py
@@ -45,7 +45,7 @@ class ExternalContractsTest(unittest.TestCase):
         self.assertEqual(str(http_error.exception), expected_exception_message)
 
     @mock.patch("requests.Session.get")
-    @mock.patch("pyobas.client.OpenBAS")
+    @mock.patch("pyoaev.client.OpenBAS")
     @mock.patch("uuid.uuid4")
     def test_when_no_preexisting_contract_create_all_from_templates(
         self, mock_uuid4, mock_obas_client, mock_requests_get
@@ -213,7 +213,7 @@ class ExternalContractsTest(unittest.TestCase):
         )
 
     @mock.patch("requests.Session.get")
-    @mock.patch("pyobas.client.OpenBAS")
+    @mock.patch("pyoaev.client.OpenBAS")
     @mock.patch("uuid.uuid4")
     def test_when_some_preexisting_contract_create_rest_from_templates(
         self, mock_uuid4, mock_obas_client, mock_requests_get
