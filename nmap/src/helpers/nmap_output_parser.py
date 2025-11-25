2637957dfc38f1f6488d0e93c19c9e40963d4992
savacano28 <stephanya.casanova@filigran.io>
2025-11-25 14:41:53 +0100

    [nmap] chore: clean code

--- nmap/src/helpers/nmap_output_parser.py
+++ nmap/src/helpers/nmap_output_parser.py
@@ -1,9 +1,14 @@
 from typing import Dict
 
+from injector_common.injector_common.targets import TargetExtractionResult
+
 
 class NmapOutputParser:
-    def parse(data: Dict, result: str, asset_list: []) -> Dict:
+    def parse(data: Dict, result: str, target_results: TargetExtractionResult) -> Dict:
         """Parse nmap results and extract open ports."""
+        asset_list = list(target_results.ip_to_asset_id_map.values())
+        targets = target_results.targets
+
         run = result["nmaprun"]
         if not isinstance(run["host"], list):
             run["host"] = [run["host"]]
@@ -27,7 +32,7 @@ class NmapOutputParser:
                             port_result["host"] = host["address"]["@addr"]
                         else:
                             port_result["asset_id"] = None
-                            port_result["host"] = asset_list[idx]
+                            port_result["host"] = targets[idx]
                         ports_scans_results.append(port_result)
 
         return {
--- nmap/src/openaev_nmap.py
+++ nmap/src/openaev_nmap.py
@@ -53,7 +53,6 @@ class OpenAEVNmap:
         target_results = Targets.extract_targets(
             selector_key, selector_property, data, self.helper
         )
-        asset_list = list(target_results.ip_to_asset_id_map.values())
         # Deduplicate targets
         targets = target_results.targets
         # Handle empty targets as an error
@@ -90,7 +89,7 @@ class OpenAEVNmap:
         jc = NmapProcess.js_execute(["jc", "--xml", "-p"], nmap_result)
         result = json.loads(jc.stdout.decode("utf-8").strip())
 
-        return NmapOutputParser.parse(data, result, asset_list)
+        return NmapOutputParser.parse(data, result, target_results)
 
     def process_message(self, data: Dict) -> None:
         start = time.time()
