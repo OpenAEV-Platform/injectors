4906059539adce50b88c5854ef799a29dc79f25f
savacano28 <stephanya.casanova@filigran.io>
2025-11-25 15:45:46 +0100

    [nmap] chore: clean code

--- nmap/src/contracts/nmap_contracts.py
+++ nmap/src/contracts/nmap_contracts.py
@@ -1,36 +1,25 @@
 from typing import List
 
-from injector_common.constants import (
-    TARGET_PROPERTY_SELECTOR_KEY,
-    TARGET_SELECTOR_KEY,
-    TARGETS_KEY,
-)
-from injector_common.targets import TargetProperty, target_property_choices_dict
+from contracts.nmap_constants import (FIN_SCAN_CONTRACT,
+                                      TCP_CONNECT_SCAN_CONTRACT,
+                                      TCP_SYN_SCAN_CONTRACT, TYPE)
 from pyoaev.contracts import ContractBuilder
-from pyoaev.contracts.contract_config import (
-    Contract,
-    ContractAsset,
-    ContractAssetGroup,
-    ContractCardinality,
-    ContractConfig,
-    ContractElement,
-    ContractExpectations,
-    ContractOutputElement,
-    ContractOutputType,
-    ContractSelect,
-    ContractText,
-    Expectation,
-    ExpectationType,
-    SupportedLanguage,
-    prepare_contracts,
-)
+from pyoaev.contracts.contract_config import (Contract, ContractAsset,
+                                              ContractAssetGroup,
+                                              ContractCardinality,
+                                              ContractConfig, ContractElement,
+                                              ContractExpectations,
+                                              ContractOutputElement,
+                                              ContractOutputType,
+                                              ContractSelect, ContractText,
+                                              Expectation, ExpectationType,
+                                              SupportedLanguage,
+                                              prepare_contracts)
 
-from contracts.nmap_constants import (
-    FIN_SCAN_CONTRACT,
-    TCP_CONNECT_SCAN_CONTRACT,
-    TCP_SYN_SCAN_CONTRACT,
-    TYPE,
-)
+from injector_common.constants import (TARGET_PROPERTY_SELECTOR_KEY,
+                                       TARGET_SELECTOR_KEY, TARGETS_KEY)
+from injector_common.targets import (TargetProperty,
+                                     target_property_choices_dict)
 
 
 class NmapContracts:
