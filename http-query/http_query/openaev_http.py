66e55180839deaa33187b23209cdb495104d898e
Antoine MAZEAS <antoine.mazeas@filigran.io>
2025-12-08 11:34:08 +0100

    je devienne fou
    Signed-off-by: Antoine MAZEAS <antoine.mazeas@filigran.io>

--- http-query/http_query/openaev_http.py
+++ http-query/http_query/openaev_http.py
@@ -3,8 +3,6 @@ import time
 from typing import Dict
 
 import requests
-from pyoaev.helpers import OpenAEVConfigHelper, OpenAEVInjectorHelper
-
 from http_query.contracts_http import (
     HTTP_FORM_POST_CONTRACT,
     HTTP_FORM_PUT_CONTRACT,
@@ -14,6 +12,7 @@ from http_query.contracts_http import (
     HttpContracts,
 )
 from http_query.helpers.helpers import HTTPHelpers
+from pyoaev.helpers import OpenAEVConfigHelper, OpenAEVInjectorHelper
 
 
 class OpenAEVHttp:
