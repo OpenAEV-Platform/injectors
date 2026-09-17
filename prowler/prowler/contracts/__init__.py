ab93000fdb4871320a5ec031a070fe6ca727571d
Christophe Melchior <christophe.melchior@filigran.io>
2026-09-17 20:58:44 +0200

    test(prowler): align registry checks with CHK.010 (#422)

--- prowler/prowler/contracts/__init__.py
+++ prowler/prowler/contracts/__init__.py
@@ -13,6 +13,7 @@ from .base import (
 from .catalog import ROUTE_CATALOG, RouteDescriptor
 from .dispatcher import ContractDispatcher, RouteHandler, RouteNotFoundError
 from .gcp import GcpBaseContract
+from .kubernetes import KubernetesBaseContract
 from .registry import (
     DEFAULT_PROWLER_CONTRACTS,
     PROWLER_CONTRACT_NAMESPACE,
@@ -25,6 +26,7 @@ __all__ = [
     "AwsBaseContract",
     "AzureBaseContract",
     "GcpBaseContract",
+    "KubernetesBaseContract",
     "ContractDispatcher",
     "ContractExecutionOutcome",
     "ContractInputError",
