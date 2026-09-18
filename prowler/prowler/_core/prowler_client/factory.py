642bd998886c6ae17eb43f74c1f5978532845e75
Christophe Melchior <christophe.melchior@filigran.io>
2026-09-18 09:41:52 +0200

    style(prowler): sort client factory imports (#422)

--- prowler/prowler/_core/prowler_client/factory.py
+++ prowler/prowler/_core/prowler_client/factory.py
@@ -8,13 +8,13 @@ from prowler.models.configs.config_loader import ProwlerConfig
 from prowler.models.provider_inputs import ProviderInput
 
 from .client import ProwlerClient
+from .credentials import TemporaryCredentialLeaseFactory
+from .output_workspace import TemporaryOutputWorkspaceFactory
 from .ports import (
     CliEngineFactoryPort,
     CredentialLeaseFactoryPort,
     OutputWorkspaceFactoryPort,
 )
-from .credentials import TemporaryCredentialLeaseFactory
-from .output_workspace import TemporaryOutputWorkspaceFactory
 from .provider_adapter import ProviderInvocationAdapter
 
 
